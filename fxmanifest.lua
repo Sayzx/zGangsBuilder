4d757bc3a096289fa2c5f021a1a5f103db303132
Sayz <74567624+Sayzx@users.noreply.github.com>
2022-07-17 16:49:35 +0200

    Patch 1.2
    Retrait du Menu F7 qui cassais tout

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,16 +7,6 @@ shared_scripts {
 }
 
 client_scripts {
-	-- "RageUI/RMenu.lua",
-    -- "RageUI/menu/RageUI.lua",
-    -- "RageUI/menu/Menu.lua",
-    -- "RageUI/menu/MenuController.lua",
-    -- "RageUI/components/*.lua",
-    -- "RageUI/menu/elements/*.lua",
-    -- "RageUI/menu/items/*.lua",
-    -- "RageUI/menu/panels/*.lua",
-    -- "RageUI/menu/windows/*.lua",
-	-- --
         'RageUI/RMenu.lua',
         'RageUI/menu/RageUI.lua',
         'RageUI/menu/Menu.lua',
@@ -26,14 +16,12 @@ client_scripts {
         'RageUI/menu/items/*.lua',
         'RageUI/menu/panels/*.lua',
         'RageUI/menu/windows/*.lua',
-        'client/f7_client.lua',
 	    "client/*.lua",
 }
 
 server_scripts {
 	"@mysql-async/lib/MySQL.lua",
 	"@es_extended/locale.lua",
-	"server/f7_server.lua",
 	"server/*lua",
 }
 
